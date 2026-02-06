bbf7aa40b6fcfb7a7650c6d3759a16b75c4b74e7
Заболотный Денис <zabolotnyj@imc-dmn.parus-s.ru>
2026-02-06 10:33:53 +0400

    init

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+import subprocess
 
 
 app = Flask(__name__)
@@ -10,6 +11,17 @@ def index():
 @app.route('/api/comm')
 def comm():
     command = request.args.get('text')
-    print(command)
+    
+    result = subprocess.run(command.split(' '), capture_output=True, text=True, encoding='utf-8')
+
+    if result.returncode == 0:
+        return result.stdout
+    else:
+        return result.stderr
+
+    print("Статус:", "Успешно" if result.returncode == 0 else "Ошибка")
+    print("Код возврата:", result.returncode)
+    print("Вывод:\n", result.stdout)
+    print("Ошибки:\n", result.stderr)
 
 app.run(host='77.222.63.95', port=5000, debug=True)
