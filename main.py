7d806c441eca035f37388279a50dd586de9aa83b
Заболотный Денис <zabolotnyj@imc-dmn.parus-s.ru>
2026-02-06 10:27:44 +0400

    init

--- main.py
+++ main.py
@@ -4,8 +4,11 @@ from flask import render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def index():
     return render_template('cli.html')
 
+@app.route('/api/ls')
+def ls():
+    return 0
 
 app.run(host='77.222.63.95', port=5000, debug=True)
