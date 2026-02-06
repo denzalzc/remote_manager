8e19efb9a3b2d3b2d13defe1eafda49634dff4fb
Заболотный Денис <zabolotnyj@imc-dmn.parus-s.ru>
2026-02-06 15:00:42 +0400

    init

--- deploy.py
+++ deploy.py
@@ -2,7 +2,7 @@ import os, subprocess, sys
 
 
 def depends():
-    os.system('touch td_data')
+    subprocess.run('touch td_data', shell=True)
 
     try:
         python_version = subprocess.check_output(['python3', '--version'], stderr=subprocess.STDOUT).decode()
