4d176e8687a6155c84e64ac645a34fda7e69d9fc
Заболотный Денис <zabolotnyj@imc-dmn.parus-s.ru>
2026-02-06 10:29:39 +0400

    init

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, request
+
 
 app = Flask(__name__)
 
@@ -7,8 +7,9 @@ app = Flask(__name__)
 def index():
     return render_template('cli.html')
 
-@app.route('/api/ls')
-def ls():
-    return 0
+@app.route('/api/comm')
+def comm():
+    command = request.args.get('text')
+    print(command)
 
 app.run(host='77.222.63.95', port=5000, debug=True)
